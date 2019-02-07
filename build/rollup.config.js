ce6afc6b705b90a59b5b6349d7349dfb63bb72fd
Joeri Bakker <bakkerjoeri@gmail.com>
2019-02-07 14:17:49 +0100

    Set up build process

---
+++ build/rollup.config.js
@@ -0,0 +1,26 @@
+import buble from 'rollup-plugin-buble';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import vue from 'rollup-plugin-vue';
+
+export default {
+    input: 'src/wrapper.js',
+    output: {
+        name: 'VueQuill',
+        exports: 'named',
+        globals: {
+            quill: 'Quill',
+        },
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        vue({
+            compileTemplate: true,
+        }),
+        buble(),
+    ],
+    external: [
+        'quill',
+    ],
+};
